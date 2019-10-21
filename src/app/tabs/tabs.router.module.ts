502472f422917988b580cbf320ff7f8c55df2736
Christian Awelakoue <awelakoue@gmail.com>
2019-10-21 21:37:37 +0200

    route to the detail page

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
             path: '',
             loadChildren: () =>
               import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+          },
+          {
+            path: 'activity-detail',
+            loadChildren: '../activity-detail/activity-detail.module#ActivityDetailPageModule'
           }
         ]
       },
