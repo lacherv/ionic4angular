91f496504f710dad80cdeed8d2b2cc8e71911375
Christian Awelakoue <awelakoue@gmail.com>
2019-10-21 20:58:17 +0200

    implement REST API

--- src/app/activity.service.ts
+++ src/app/activity.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActivityService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
 
-  getActivity() {}
+  getActivity(activityID: string) {
+    // tslint:disable-next-line: no-use-before-declare
+    return this.httpClient.get(API + '/id/' + activityID);
+  }
 
-  getAllActivities() {}
+  getAllActivities() {
+    // tslint:disable-next-line: no-use-before-declare
+    return this.httpClient.get(API);
+  }
 }
+
+// Declare a constant for a API.
+const API = 'https://orangevalleycaa.org/api/videos';
