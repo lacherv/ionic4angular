42d7152830806ef818cf0e11c9a2ffbecdaf409c
Christian Awelakoue <awelakoue@gmail.com>
2019-10-21 21:17:39 +0200

    finalize the home page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Activity } from '../types';
+import { ActivityService } from '../activity.service';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +9,9 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  activityList: Observable<Activity[]>;
 
-  constructor() {}
-
+  constructor(activityService: ActivityService) {
+    this.activityList = activityService.getAllActivities();
+  }
 }
